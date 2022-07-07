3f79954c2e9db1d239316030957c89ff6ffeda60
Chiamaka Ugwu <mannehills@gmail.com>
2022-07-07 17:06:48 +0100

    Update

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var pop_up_reg = document.getElementById('pop-up-reg');
+var pop_up_reg2 = document.getElementById('pop-up-reg2');
 var register = document.getElementById('register');
 var register2 = document.getElementById('register2');
 var register3 = document.getElementById('register3');
 var register4 = document.getElementById('register4');
+var register5 = document.getElementById('register5');
+var register6 = document.getElementById('register6');
+var register7 = document.getElementById('register7');
+var training = document.getElementById('training');
+
 var reg_close = document.getElementById('reg-close');
+var reg_close2 = document.getElementById('reg-close2');
+
 
 var pop_up_call = document.getElementById('pop-up-call');
 var call = document.getElementById('call');
@@ -13,21 +21,38 @@ var call_close = document.getElementById('call-close');
 
 
 register.addEventListener('click', function(){
-    pop_up_reg.style.display = "flex";
+    pop_up_reg2.style.display = "flex";
 })
 register2.addEventListener('click', function(){
     pop_up_reg.style.display = "flex";
 })
 register3.addEventListener('click', function(){
-    pop_up_reg.style.display = "flex";
+    pop_up_reg2.style.display = "flex";
 })
 register4.addEventListener('click', function(){
+    pop_up_reg2.style.display = "flex";
+})
+register5.addEventListener('click', function(){
     pop_up_reg.style.display = "flex";
+    training.setAttribute("value", "Product Management");
+})
+register6.addEventListener('click', function(){
+    pop_up_reg.style.display = "flex";
+    training.setAttribute("value", "Product Marketing");
+
+})
+register7.addEventListener('click', function(){
+    pop_up_reg.style.display = "flex";
+    training.setAttribute("value", "Product Testing");
+
 })
 
 
 reg_close.addEventListener('click', function(){
     pop_up_reg.style.display = "none"
+})
+reg_close2.addEventListener('click', function(){
+    pop_up_reg2.style.display = "none"
 })
 
 
